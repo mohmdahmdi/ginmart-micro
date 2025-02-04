adfca5325e1a735148a7f7929a700ec0418dba8f
callmeJOUYA <mohammad.pr285@gmail.com>
2025-02-04 16:05:55 +0330

    updating main.go file

--- main.go
+++ main.go
@@ -1,28 +1,33 @@
 package main
 
 import (
+	"go-micro/config"
 	"go-micro/controllers"
 	"go-micro/middlewares"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    router := gin.Default()
+  config.InitDB()
+  defer config.CloseDB()
 
-    router.Use(middlewares.LoggerMiddleWare())
+  router := gin.Default()
 
-    router.POST("/auth/register", controllers.Register)
-    router.POST("/auth/login", controllers.Login)
+  router.Use(middlewares.LoggerMiddleWare())
 
-    router.POST("/products", middlewares.AuthMiddleware(), controllers.AddProduct) 
-    router.GET("/products", controllers.GetProducts)
-    router.GET("/products/:id", controllers.GetProduct)
-    router.PUT("/products/:id", middlewares.AuthMiddleware(), controllers.UpdateProduct)
-    router.DELETE("/products/:id", middlewares.AuthMiddleware(), controllers.DeleteProduct)
+  router.POST("/auth/register", controllers.Register)
+  router.POST("/auth/login", controllers.Login)
 
-    router.POST("/products/:id/reviews", middlewares.AuthMiddleware(), controllers.AddReview)
-    router.GET("/products/:id/reviews", controllers.GetReviews)
-		router.DELETE("/products/:id/reviews/:id", controllers.DeleteReview)
+  router.POST("/products", middlewares.AuthMiddleware(), controllers.AddProduct) 
+  router.GET("/products", controllers.GetProducts)
+  router.GET("/products/:id", controllers.GetProduct)
+  router.PUT("/products/:id", middlewares.AuthMiddleware(), controllers.UpdateProduct)
+  router.DELETE("/products/:id", middlewares.AuthMiddleware(), controllers.DeleteProduct)
 
-    router.Run()
+  router.POST("/products/:id/reviews", middlewares.AuthMiddleware(), controllers.AddReview)
+  router.GET("/products/:id/reviews", controllers.GetReviews)
+  router.DELETE("/products/:id/reviews/:id", controllers.DeleteReview)
+
+  router.Run()
 }
