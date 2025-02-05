1319f429a5d693b87b9a932825262e2518398de8
callmeJOUYA <mohammad.pr285@gmail.com>
2025-02-05 15:30:39 +0330

    seprating authentication miidlewares

---
+++ middlewares/authMiddlewares.go
@@ -0,0 +1,16 @@
+package middlewares
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		apiKey := c.GetHeader("X-API-Key")
+		if apiKey == "" {
+			c.AbortWithStatusJSON(401, gin.H{"error": "Unauthorized"})
+			return
+		}
+		c.Next()
+	}
+}
