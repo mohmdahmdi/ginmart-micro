ad2135b72f2bd7b72020b39c79429fd0548afba9
callmeJOUYA <mohammad.pr285@gmail.com>
2025-02-06 16:09:47 +0330

    working on login controller

--- controllers/authControllers.go
+++ controllers/authControllers.go
@@ -1,19 +1,23 @@
 package controllers
 
 import (
+	"database/sql"
 	"go-micro/config"
 	"go-micro/models"
 	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
 	"golang.org/x/crypto/bcrypt"
 )
 
 func Register(c *gin.Context) {
 	var user models.User
-
-	if err := c.ShouldBindJSON(&user); err != nil {
+	err := c.ShouldBindJSON(&user)
+	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request"})
 		return
 	}
@@ -25,7 +29,7 @@ func Register(c *gin.Context) {
 		return
 	}
 
-	query := `INSERT INTO users (username, password, role) VALUES ($1, $2, $3) RETURNING id`
+	query := `INSERT INTO users (username, password_hash, role) VALUES ($1, $2, $3) RETURNING id`
 	var userID int
 	err = config.DB.QueryRow(query, user.Username, string(hashedPassword), user.Role).Scan(&userID)
 	if err != nil {
@@ -41,4 +45,58 @@ func Register(c *gin.Context) {
 }
 
 
-func Login(c *gin.Context) {}
+func Login(c *gin.Context) {
+	var user models.User
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request"})
+		return
+	}
+
+	query := `SELECT id, username, password_hash, role FROM users WHERE username = $1`
+	var storedUser models.User
+	var hashedPassword string
+	err := config.DB.QueryRow(query, user.Username).Scan(&storedUser.ID, &storedUser.Username, &hashedPassword, &storedUser.Role)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid username or password"})
+			return
+		}
+		log.Println("Error fetching user:", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Server error"})
+		return
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(user.Password))
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid username or password"})
+		return
+	}
+
+	secretKey := os.Getenv("JWT_SECRET")
+	if secretKey == "" {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Secret key not found"})
+		return
+	}
+
+	claims := &jwt.RegisteredClaims{
+		Subject:   string(rune(storedUser.ID)),
+		Issuer:    "ginmart-micro",
+		IssuedAt:  jwt.NewNumericDate(time.Now()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString([]byte(secretKey))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to generate token"})
+		return
+	}
+
+	c.SetCookie("token", tokenString, 3600, "/", "", false, true)
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Login successful",
+		"token":   tokenString,
+	})
+}
