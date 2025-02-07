be60b2c2fcafd4acace0a7f70c97db01fad6a5e7
callmeJOUYA <mohammad.pr285@gmail.com>
2025-02-07 15:33:37 +0330

    defining new claims struct to include user id and role

--- models/user.go
+++ models/user.go
@@ -1,8 +1,16 @@
 package models
 
+import "github.com/golang-jwt/jwt/v5"
+
 type User struct {
 	ID       int    `json:"id"`
 	Username string `json:"username"`
 	Password string `json:"password"`
 	Role     string `json:"role"`
+}
+
+type Claims struct {
+	UserID int    `json:"user_id"`
+	Role   string `json:"role"`
+	jwt.RegisteredClaims
 }
